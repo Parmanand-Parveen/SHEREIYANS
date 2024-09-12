0eafb042581fcb4169bd9f242eb1d0f0188bf20f
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-09-12 20:32:19 +0530

    Input output

---
+++ 15.Input_Output/script.js
@@ -0,0 +1,33 @@
+//output
+// console.log("Hello")
+// alert("Hello")
+
+
+// input
+
+// var n = prompt("Enter a number")
+// var m = prompt("Enter a number")
+// console.log(m+n)
+// console.log(typeof n)
+// var res = confirm("Do you want to leave this page")
+
+
+// Type conversion
+
+// Explicit type conversion
+
+// var n = Number(prompt("Enter a number"))
+// var m = +prompt("Enter a number")
+
+// console.log(m+n)
+// console.log(typeof m)
+
+// Implicit type conversion
+
+// 12+"Hello"="12Hello"
+TODO 
+// JIT compiler
+
+
+
+
