bad7a8f935250b9ed8d5fbd50b88a4c195cdc1c0
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-09-13 20:34:09 +0530

    Operators

---
+++ 16.Operators/script.js
@@ -0,0 +1,45 @@
+// 1.Assignment Operator
+
+{
+    let m = 15
+    let n = 5
+    let sum = m+n
+    console.log(`the sum of ${m} and ${n} is ${sum}`)
+}
+
+{
+    let m = 15
+    let n = 5
+    let sub = m-n
+    console.log(`the subtraction of ${m} and ${n} is ${sub}`)
+}
+
+{
+    let m = 15
+    let n = 5
+    let mul = m*n
+    console.log(`the multiplication of ${m} and ${n} is ${mul}`)
+}
+
+{
+    let m = 15
+    let n = 5
+    let div = m/n
+    console.log(`the division of ${m} and ${n} is ${div}`)
+}
+
+{
+    let m = 15
+    let n = 5
+    let mod = m%n
+    console.log(`the modulus of ${m} and ${n} is ${mod}`)
+
+    // Whenever we modulo a small number by a large number it gives us the small number as the answer.
+}
+
+{
+    let m = 15
+    let n = 5
+    let exp = m**n
+    console.log(`the exponential of ${m} and ${n} is ${exp}`)   
+}
