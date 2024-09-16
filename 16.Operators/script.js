bed7d1fcef55faaf25eb3aa0d47e18b89970e571
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-09-16 20:26:33 +0530

    Some more opeartors

--- 16.Operators/script.js
+++ 16.Operators/script.js
@@ -1,4 +1,4 @@
-// 1.Assignment Operator
+// 1. Arithmetic Operators
 
 {
     let m = 15
@@ -43,3 +43,34 @@
     let exp = m**n
     console.log(`the exponential of ${m} and ${n} is ${exp}`)   
 }
+
+//02. pre/post Increment/decrement
+{let a =12;
+
+let res = ++a + ++a + ++a + a--
+
+console.log(res)
+console.log(a)}
+
+// 03. Comparison operators
+
+// == >= <= != > <  === !==
+
+{let a = 10;
+let b = "10"
+
+console.log(a === b)}
+
+
+//When we compare string with comparison operators , only the first non equal character is compared according to it's ASCII value
+
+{
+
+    let a= "Hiii"
+    let b = "Hello"
+    console.log(a > b)
+}
+
+// 04. Logical operators 
+
+//  &&  ||  !  result (according to the data which is logically compared)
