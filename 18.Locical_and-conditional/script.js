6e6d8c0b2763e4759270d2cb542ba9cfe51f9971
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-09-19 20:28:26 +0530

    Logical+Conditional

---
+++ 18.Locical_and-conditional/script.js
@@ -0,0 +1,63 @@
+// let n = 0
+
+
+// if(n>0 && n%2 == 0){
+//     console.log("positive and even")
+// }else if(n>0 && n%2 != 0){
+//     console.log("positive and odd")
+// }else if(n<0 ){
+//     console.log("negative")
+// }else{
+//     console.log("zero")
+// }
+
+
+// How switch works
+
+// switch (condition) {
+//     case 1:
+//         // code block
+//         break;
+//     case 2:
+//         // code block
+//         break;
+//     default:
+//         // code block
+// }
+
+// TODO
+// What is fall throw
+
+// let name =  prompt("Enter Your Name")
+
+// let age = +prompt("Enter Your Age")
+
+// console.log(name,age)
+
+
+// const res =age>18 ?"You can vote":`You are not eligible for vote for more ${18 - age} years`
+
+// console.log(res)
+
+// if (Number(age) >= 18) {
+//     console.log("You can vote")}
+//   else{
+//     console.log( `You are not eligible for vote for more ${18 - age} years`)
+//   }  
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+let a = prompt("Enter first number")
+let b = prompt("Enter second number")
+let c = prompt("Enter third number")
+
+
+if(a ==b &&  b==c){
+    console.log("All numbers are equal")
+} else if(a==b || b==c || c==a){
+    console.log("Two numbers are equal")    
+}else{
+    console.log("All numbers are different")
+}
