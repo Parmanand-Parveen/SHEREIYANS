8ea5877bf3425af9231c870476cc4382e7a1e39c
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-09-20 23:58:41 +0530

    loops ✅

---
+++ 19.Loops/script.js
@@ -0,0 +1,27 @@
+//Iterative statement 
+// Entry control loop(while, for)
+// Exit control loop(do while)
+
+// for (let index = 1; index <= 10; index++) {
+//  console.log(index)
+// }
+
+let i = 1
+// while(i<=10){
+//     console.log(i)
+//     i++
+// }
+
+// do {
+//     console.log(i)
+//     i++;
+// } while (1<=10);
+
+// for (let index = 1; index <= 10; index++) {
+//     if (index%2 != 0) {
+//         continue
+//     }
+//     console.log(index)
+// }
+
+// console.log("End of loop")
