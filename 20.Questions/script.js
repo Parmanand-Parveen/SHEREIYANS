06411c606fecd23059a47087c88815a7c184d522
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-09-23 20:24:41 +0530

    Questions

---
+++ 20.Questions/script.js
@@ -0,0 +1,29 @@
+// let a = +prompt("Enter first number")
+// let b = +prompt("Enter second number")
+
+
+// console.log(a+b)
+// console.log(`before swapping ${a} ${b}`)
+
+
+// a = a+b  // 11+10 = 21
+// b = a-b  // 21-10 = 11 
+// a = a-b   //21-11 = 10
+
+// console.log(`after swapping ${a} ${b}`)
+
+
+// let a = +prompt("Enter the length of rectangle")
+// let b = +prompt("Enter the brath of rectangle")
+// console.log(`The area of rectangle is ${a*b}`)
+
+
+// let p = +prompt("Enter the principle amount")
+// let r = +prompt("Enter the rate of interest")
+// let t = +prompt("Enter the time in years")
+// let si = (p*r*t)/100
+// console.log(`The simple interest is ${si}`)
+
+//Accept the  3 sides of a triangle and find the area by herons formula
+//Find the circumfrence of a sphere
+// Find the area and parameter of the circle
