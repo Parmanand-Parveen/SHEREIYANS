a03db7239b17743b9242cbf1a47aa51a650f4d62
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-09-24 23:29:42 +0530

    Function

---
+++ 21.Function/script.js
@@ -0,0 +1,28 @@
+// Function 
+
+//Function is a block of code which only runs when it is called
+//Function is a subprogram that performs a particular task
+
+//Here we are using function paradigm
+
+// function functionname(){
+//     console.log("Hello")
+// }
+
+// functionname()
+
+
+// function functionname(){
+//     console.log("Hello")
+// }
+
+// functionname()
+// let x= functionname()
+
+//console.log(x)
+
+//1. By default function returns undefined
+//2. Return line be the last line of the function
+//3. Only single entity must be returned by the function 
+//4. Function call is repalaced by its return value
+//5. CODE written after return in the function wull be not reachable or be ignored 
