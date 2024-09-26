243c7ace2c50c8fafa823e2edf4c3673d27cd30d
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-09-26 19:24:18 +0530

    Concept++

--- 21.Function/script.js
+++ 21.Function/script.js
@@ -26,3 +26,52 @@
 //3. Only single entity must be returned by the function 
 //4. Function call is repalaced by its return value
 //5. CODE written after return in the function wull be not reachable or be ignored 
+
+
+// function func_add(a, b){
+
+//    sum = a+b
+//    console.log(sum)
+// }
+
+
+//  func_add("b",2)
+
+// +++++++++++++++++++++++++++++++++++++++++++++++25/09/2024++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+ // Return keyword kisi bhi function ke andar hue operation ko bahar nikalne ke liye use hota hai
+
+ // Function -> not compulsory to return anything
+ // Method(object)-> compulsory to return something
+
+//  function getName(){
+//     console.log("John")
+//  }
+ 
+
+//  getName() // our function call (non parameterized function)
+//  console.log("Hello")   //Js function call (parameterized function call)
+
+// function getName(name){ //parameter(s)
+//    console.log(name) 
+// }
+
+// getName("John") // argument(s)
+
+// function getname(name,city){
+//    console.log(n,c)
+// }
+
+// let n = "John"
+// let c = "Delhi"
+// getname(n,c)
+
+function getsum(a,b){
+   sum = a+b
+    return sum
+}
+
+let res = getsum(2,3)
+let multiple = 5
+
+console.log(res*multiple)
