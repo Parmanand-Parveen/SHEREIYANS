d783de999a3c349025a9ffa014db7e8c006c70d7
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-10-01 22:47:33 +0530

    Premimitive Vs Reference

---
+++ 22.Premitive vs Reference/script.js
@@ -0,0 +1,18 @@
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// var a = 10
+// let b = 20
+// const c = 30
+
+// ans(2,3)
+// const ans = (a,b)=>{   
+//     let sum = a+b
+//     return sum
+// }
+
+// 1. variable with var keyword shows partial hoisting
+// 2. variable with let keyword shows no hoisting temporal dead zone(TDZ)
+// 3. Function with function keyword shows full hoisting
+// 4. Function with arrow function shows no hoisting(TDZ) or depending on the way it is declared
