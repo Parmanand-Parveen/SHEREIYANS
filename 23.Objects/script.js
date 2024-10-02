ea636f4a9ecc4403d6037aa0c1c7930c74f907d3
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-10-02 20:27:23 +0530

    Learned Objects

---
+++ 23.Objects/script.js
@@ -0,0 +1,64 @@
+// const profile = {
+//     name:"Alex",
+//     age:20
+// }
+
+
+// console.log(profile.name)
+// console.log(profile["age"])
+// console.log(profile)
+
+// //upadating existing property
+// console.log("Upadating existing property")
+// profile.age = 55
+// profile["name"] = "John"
+
+// console.log(profile)
+
+// //adding new property
+// console.log("Adding new property")
+
+// profile.gender = "male"
+// profile.city = "Delhi"
+// console.log(profile)
+// //deleting property
+// console.log("After deletion")
+// delete profile.age
+// console.log(profile)
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// const profile = {
+//     name : "Alex",
+//     age : 20,
+//     isStudent : true,
+//     city: "Delhi",
+// }
+
+
+// //traversing an object
+
+// for(let key in profile){
+
+//     console.log(key,"=>",profile[key])
+// }
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Coping the object
+
+// const profile1 = {
+//     name : "Alex",
+//     age : 20,
+// }
+
+// const profile2 = { ...profile1}   //spread operator
+// console.log("coping the profile")
+// console.log(profile1)
+// console.log(profile2)
+
+// profile2.name = "John"
+
+// console.log(profile1)
+// console.log("After updating profile2")
+// console.log(profile2)
