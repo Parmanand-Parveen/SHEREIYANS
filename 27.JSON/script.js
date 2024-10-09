356d41477a9da9e7a6fb9905838c1bcac68a97d8
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-10-09 20:28:16 +0530

    Learnt JSON

---
+++ 27.JSON/script.js
@@ -0,0 +1,14 @@
+// // console.log("Hello World")
+
+// let arr= [{},{},{}]
+
+// console.log(typeof arr)
+
+
+// let resjson = JSON.stringify(arr) //JSON->string
+
+// console.log(typeof resjson)
+
+// let resobj = JSON.parse(resjson) //string->JSON
+
+// console.log(typeof resobj) 
