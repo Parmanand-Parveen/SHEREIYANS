9f2a2932527ac0b1099822b8bfb2e50c29bdc752
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-10-10 22:07:08 +0530

    Learnt String

---
+++ 28.String/script.js
@@ -0,0 +1,35 @@
+// let s = "Hello"
+
+// console.log(s)
+// console.log(s[2])
+// console.log(s.at(-1))
+
+
+
+//Strings can be reassigned but not updateable
+
+// s= "hiiii"
+
+// console.log(s)
+
+// s=null
+
+// console.log(s)
+
+//String traversal
+
+let s = "Hello world"
+
+// for(let i=0;i<s.length;i++){
+//     console.log(`Alphabet at index ${i} is ${s[i]}`)
+// }
+
+// String methods
+//s+"hiii"
+
+// console.log(s.concat("duniya"))
+// console.log(s.toUpperCase())
+// console.log(s.toLowerCase())
+// console.log(s.replace("H","J"))
+// console.log(s.split(" "))
+
