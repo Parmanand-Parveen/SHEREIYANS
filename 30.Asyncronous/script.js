be36725d3696d4c5628c4fb1e66ced52a25c2b0d
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-10-16 19:44:34 +0530

    Async

---
+++ 30.Asyncronous/script.js
@@ -0,0 +1,23 @@
+// console.log(1)
+
+
+// setTimeout(() => {
+//     console.log(2)
+// }, 1000);
+
+
+// console.log(3)
+
+// let count = 0
+// const x = setInterval(() => {
+//     console.log(count++)
+//     if(count == 5){
+
+//         clearInterval(x)
+//           console.log("Count stoped due to clearInterval") 
+//     }
+ 
+    
+// }, 1000)
+
+
