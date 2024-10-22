89446aee5364ffc279c7ba8ac805c7d64587cd86
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-10-22 15:50:07 +0530

    Reviseted DOM✅

---
+++ 32.DOM/script.js
@@ -0,0 +1,16 @@
+// Document- "document " is the predefined object given by the Js which holde the connected html file (inde.html)
+
+// console.log(document)
+
+// id - document.getElementById()
+// class - document.getElementsByClassName()
+// tag - document.getElementsByTagName()
+
+
+// document.querySelector() // to select the first/single element 
+// document.querySelectorAll() // to select all elements
+
+// const header = document.querySelector('#heading'); // selects the element with id 'heading'
+// header.innerHTML= "Bye bye " // changes the innerHtml of the header
+// header.style.color ='red';  // changes the color of the header
+// header.remove()  // removes the header from the DOM
