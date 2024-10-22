a3c84cf4e200de9815fb0c615ece4fcc51892904
Parmanand-Parveen <parmanandparveen@gmail.com>
2024-10-22 20:25:20 +0530

    +++

--- 32.DOM/script.js
+++ 32.DOM/script.js
@@ -6,11 +6,26 @@
 // class - document.getElementsByClassName()
 // tag - document.getElementsByTagName()
 
-
-// document.querySelector() // to select the first/single element 
+// document.querySelector() // to select the first/single element
 // document.querySelectorAll() // to select all elements
 
-// const header = document.querySelector('#heading'); // selects the element with id 'heading'
+// const header = document.querySelector("#heading"); // selects the element with id 'heading'
 // header.innerHTML= "Bye bye " // changes the innerHtml of the header
 // header.style.color ='red';  // changes the color of the header
 // header.remove()  // removes the header from the DOM
+
+// const button = document.querySelector(".changeheading");
+// const resetButton = document.querySelector(".reset");
+
+// button.addEventListener("click", function () {
+//   header.innerHTML = "Hello World!";
+//   header.style.color = "red";
+//   header.style.fontSize = "24px";
+//   header.style.fontWeight = "bold";
+//   header.style.textTransform = "uppercase";
+//   header.style.textDecoration = "underline";
+// });
+
+// resetButton.addEventListener("click", ()=> {
+//   window.location.reload();
+// });
